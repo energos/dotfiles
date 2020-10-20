4594ce0f0dbf9262aaaa1920f955d39a110a43a8
energos <energos@users.noreply.github.com>
2020-10-20 01:50:02 -0300

    emacs/init.el: Reduce ivy-switch-buffer clutter

--- emacs/init.el
+++ emacs/init.el
@@ -225,11 +225,11 @@
   (setq ivy-rich-display-transformers-list
         '(ivy-switch-buffer
           (:columns
-           ((ivy-switch-buffer-transformer (:width 30)) ; add face by the original transformer
-            (ivy-rich-switch-buffer-size (:width 7)) ; return buffer size
+           ((ivy-switch-buffer-transformer (:width 20)) ; add face by the original transformer
+            ;; (ivy-rich-switch-buffer-size (:width 7)) ; return buffer size
             (ivy-rich-switch-buffer-indicators (:width 4 :face error :align right)) ; return buffer indicator
-            (ivy-rich-switch-buffer-major-mode (:width 12 :face warning)) ; return major mode info
-            (ivy-rich-switch-buffer-project (:width 15 :face success)) ; return project name `projectile'
+            ;; (ivy-rich-switch-buffer-major-mode (:width 12 :face warning)) ; return major mode info
+            (ivy-rich-switch-buffer-project (:width 10 :face success)) ; return project name `projectile'
             (ivy-rich-switch-buffer-path (:width (lambda (x) (ivy-rich-switch-buffer-shorten-path x (ivy-rich-minibuffer-width 0.3)))))) ; return file path relative to project root or `default-directory' if project is nil
            :predicate
            (lambda (cand) (get-buffer cand)))
