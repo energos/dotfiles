70c68d1e3c43f55d9f222680380ee8fe7d22b872
energos <energos@users.noreply.github.com>
2021-10-07 14:06:55 -0300

    Openbox menu: Add VMware entry

--- openbox/menu_virtualization.sh
+++ openbox/menu_virtualization.sh
@@ -7,6 +7,11 @@ crappy=yes
 cat <<EOF
 <openbox_pipe_menu>
 EOF
+[[ -x $(command -v vmplayer) ]] && cat <<EOF && crappy=no
+<item label="VMware Workstation Player" icon="/usr/share/icons/hicolor/48x48/apps/vmware-player.png">
+ <action name="Execute"><command>vmplayer</command></action>
+</item>
+EOF
 [[ -x $(command -v VirtualBox) ]] && cat <<EOF && crappy=no
 <item label="VirtualBox" icon="${ICONS}/apps/48/virtualbox.svg">
  <action name="Execute"><command>VirtualBox</command></action>
