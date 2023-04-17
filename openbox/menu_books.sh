0c0bf452c73cb2f710fb74798396b54300a01873
energos <energos@users.noreply.github.com>
2023-04-17 11:12:14 -0300

    Openbox recent ebooks: List okular or zathura recent documents

--- openbox/menu_books.sh
+++ openbox/menu_books.sh
@@ -34,7 +34,7 @@ do
 EOF
 done
 
-# Get recent files, zathura only
+# Get recent files, okular and zathura only
 if [[ "$BOOKS" == "${HOME}/Books" ]]; then
     cat <<EOF
 <separator />
@@ -42,17 +42,34 @@ EOF
     cat <<EOF
 <menu id="RecentFiles" label="Recent Files" icon="${OS_ICONS}/places/32/folder-recent.svg">
 EOF
-    awk 'BEGIN { RS=""; FS="\n" } /^[^#]/ { print $11 " " $1 }' "$HOME/.local/share/zathura/history" | sort -r | awk -F '[\\[\\]]' '$2 ~ /^\// { print $2 }' | head -n 10 | while read -r book
-    do
-        shortname
-        if [[ -f "$book" ]]; then
-            cat <<EOF
+    case $(xdg-mime query default application/pdf) in
+        *zathura*)
+            awk 'BEGIN { RS=""; FS="\n" } /^[^#]/ { print $11 " " $1 }' "$HOME/.local/share/zathura/history" | sort -r | awk -F '[\\[\\]]' '$2 ~ /^\// { print $2 }' | head -n 10 | while read -r book
+            do
+                shortname
+                if [[ -f "$book" ]]; then
+                    cat <<EOF
 <item label="${name}" icon="${USER_ICONS}/pdf.svg">
   <action name="Execute"><command>xdg-open "${book}"</command></action>
 </item>
 EOF
-        fi
-    done
+                fi
+            done
+            ;;
+        *)
+            grep ^File ~/.config/okularrc | sed 's/File//g' | sort -n -r | awk -F '=' '{ print $2 }' | envsubst | while read -r book
+            do
+                shortname
+                if [[ -f "$book" ]]; then
+                    cat <<EOF
+<item label="${name}" icon="${USER_ICONS}/pdf.svg">
+  <action name="Execute"><command>xdg-open "${book}"</command></action>
+</item>
+EOF
+                fi
+            done
+            ;;
+    esac
     cat<<EOF
 </menu>
 EOF
@@ -63,6 +80,5 @@ cat <<EOF
 EOF
 
 # WARNING: weird characters like '\' and '!' are problematic
-# TODO:    fix crashes with weird characters in zathura history file
+# TODO:    fix crashes with weird characters in zathura history file - xargs? - maybe fixed
 # TODO:    https://github.com/koalaman/shellcheck/wiki/SC2012
-# TODO:    okular recent files
